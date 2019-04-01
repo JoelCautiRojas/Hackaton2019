35668fc766c032b496ec0fd97a1ab1a3ff7b5df6
JoelCautiRojas <joel.cauti@gmail.com>
2019-04-01 00:10:02 -0500

    first commit

---
+++ app/src/main/java/example/com/hackaton2019/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package example.com.hackaton2019.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import example.com.hackaton2019.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
